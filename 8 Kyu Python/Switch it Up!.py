6f0f290044ecb4164a8a623686c1beb5efc1d51c
jamestjy <97773112+jamestjy@users.noreply.github.com>
2023-03-23 10:03:51 +0800

    Create Switch it Up!.py

---
+++ 8 Kyu Python/Switch it Up!.py
@@ -0,0 +1,32 @@
+def switch_it_up(number):
+    #your code here
+    match number:
+        case 0:
+            return "Zero"
+        case 1:
+            return "One"
+        case 2:
+            return "Two"
+        case 3:
+            return "Three"
+        case 4:
+            return "Four"
+        case 5:
+            return "Five"
+        case 6:
+            return "Six"
+        case 7:
+            return "Seven"
+        case 8:
+            return "Eight"
+        case 9:
+            return "Nine"
+
+"""
+  ____                                                _               _       __              
+ / _  |__ _  ___  _ __ __ _ _ __   ___ __ _  ___ __ _(_) __ _ _ __   (_)___  |_ \ _   _  __ _ 
+| (_| |__` |/ _ \| '_ |__` | '_ \ / _ ' _` |/ _ ' _` | |/ _` | '_ \  | |__ \  _| | | | |/ _` |
+ \__  |  | | (_) | |_) | | | |_) | | | | | | | | | | | | | | | |_) | | / __/ |_  | |_| | | | |
+    |_|  |_|\___/| .__/  |_|_.__/|_| |_| |_|_| |_| |_|_|_| |_| .__/  |_\___|   |_|_.__/|_| |_|
+                  \___|                                       \___|                                   
+"""
