c29f42a73386002895d8a2b6fc4c8c5b64b50ac9
jamestjy <97773112+jamestjy@users.noreply.github.com>
2023-09-19 00:24:34 +0800

    Create Welcome!.py

---
+++ 8 Kyu Python/Welcome!.py
@@ -0,0 +1,20 @@
+def greet(language):
+    #your code here
+    return {
+        'czech': 'Vitejte',
+        'danish': 'Velkomst',
+        'dutch': 'Welkom',
+        'estonian': 'Tere tulemast',
+        'finnish': 'Tervetuloa',
+        'flemish': 'Welgekomen',
+        'french': 'Bienvenue',
+        'german': 'Willkommen',
+        'irish': 'Failte',
+        'italian': 'Benvenuto',
+        'latvian': 'Gaidits',
+        'lithuanian': 'Laukiamas',
+        'polish': 'Witamy',
+        'spanish': 'Bienvenido',
+        'swedish': 'Valkommen',
+        'welsh': 'Croeso'
+    }.get(language, 'Welcome')
