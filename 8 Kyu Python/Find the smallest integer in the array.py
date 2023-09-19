93bf6bcf8a245875e3837f500283dc7d30a5b604
jamestjy <97773112+jamestjy@users.noreply.github.com>
2023-09-19 17:56:17 +0800

    Update Find the smallest integer in the array.py

--- 8 Kyu Python/Find the smallest integer in the array.py
+++ 8 Kyu Python/Find the smallest integer in the array.py
@@ -1,7 +1,2 @@
 def find_smallest_int(arr):
-    # Code here
-    small = arr[0];
-    for i in range(0, len(arr)):
-        if(arr[i] < small):    
-            small = arr[i];
-    return small
+    return min(arr)
