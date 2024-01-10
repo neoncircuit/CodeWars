2a0088b39ef02c08674c95f5ecea3a24a372a216
jamestjy <97773112+jamestjy@users.noreply.github.com>
2024-01-10 14:56:05 +0800

    Create Safen User Input Part I - htmlspecialchars.py

---
+++ 8 Kyu Python/Safen User Input Part I - htmlspecialchars.py
@@ -0,0 +1,6 @@
+def html_special_chars(data):
+    data = data.replace("&", "&amp;")
+    data = data.replace("<", "&lt;")
+    data = data.replace(">", "&gt;")
+    data = data.replace('"', "&quot;")
+    return data
