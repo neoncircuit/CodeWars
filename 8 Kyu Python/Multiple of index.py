96981d287e9882ead76ff375ed493a9e446d466e
neoncircuit <97773112+neoncircuit@users.noreply.github.com>
2024-12-24 23:12:39 +0800

    Update Multiple of index.py

--- 8 Kyu Python/Multiple of index.py
+++ 8 Kyu Python/Multiple of index.py
@@ -1,6 +1,9 @@
 def multiple_of_index(arr):
     result = []
-    for i in range(1, len(arr)):  
-        if arr[i] % i == 0:
+    for i in range(len(arr)): 
+        if i == 0: 
+            if arr[i] == 0:
+                result.append(arr[i])
+        elif arr[i] % i == 0:
             result.append(arr[i])
     return result
