cd7966a4766cc86394608d37be6f211425c5d10e
neoncircuit <97773112+neoncircuit@users.noreply.github.com>
2024-12-25 12:10:58 +0800

    Added solution code for Pythagorean Triple (8 Kyu)

---
+++ 8 Kyu Python/Pythagorean Triple.py
@@ -0,0 +1,6 @@
+# input is an unsorted list of 3 positive integers
+def pythagorean_triple(integers):
+    # return True if it is possible to form a Pythagorean triple with the 3 integers
+    # return False if it is not possible
+    a, b, c = sorted(integers)
+    return a**2 + b**2 == c**2
