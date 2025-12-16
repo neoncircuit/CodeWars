c7211c32a9ef6c4c22a92202c409527c706cd4d4
neoncircuit <97773112+neoncircuit@users.noreply.github.com>
2025-12-15 19:51:49 -1000

    Add Python solution: Grasshopper - Debug (8 Kyu)

---
+++ 8 Kyu Python/Grasshopper - Debug.py
@@ -0,0 +1,10 @@
+def weather_info(temp: float) -> str:
+    c = convert_to_celsius(temp)
+    if (c < 0):
+        return f"{c} is freezing temperature"
+    else:
+        return f"{c} is above freezing temperature"
+    
+def convert_to_celsius(temperature: float) -> float:
+  celsius: float = (temperature - 32) * (5/9)
+  return celsius
