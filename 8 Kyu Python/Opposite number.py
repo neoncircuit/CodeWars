89c992c5fd54bbdf182ce2e7e85b9e3cb9273cf7
neoncircuit <97773112+neoncircuit@users.noreply.github.com>
2026-01-27 20:27:19 -1000

    Add Python solution: Opposite number (8 Kyu)

---
+++ 8 Kyu Python/Opposite number.py
@@ -0,0 +1,12 @@
+def opposite(number: int) -> int:
+  # your solution here
+    """
+    Return the opposite of a given number.
+
+    Args:
+        number: int - The input number.
+
+    Returns:
+        The inverse of the input number. Eg. -34 returns 34, 1 returns -1, etc.
+    """
+    return -number
