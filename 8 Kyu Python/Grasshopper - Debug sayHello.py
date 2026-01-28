ed4b8b69fc67f47e1eae65039992c9349385e0f2
neoncircuit <97773112+neoncircuit@users.noreply.github.com>
2026-01-27 22:24:38 -1000

    Add Python solution: Grasshopper - Debug sayHello (8 Kyu)

---
+++ 8 Kyu Python/Grasshopper - Debug sayHello.py
@@ -0,0 +1,11 @@
+def say_hello(name: str) -> str:
+    """
+    Fix code to greeting program.
+    
+    Args:
+        - name: str - Name of the user
+
+    Returns:
+        Hello, {name}
+    """
+    return f"Hello, {name}"
